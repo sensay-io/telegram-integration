e13df05fa80bb1c65ff1c41510a54503509b1e2b
vpashkov <vadim@sensay.io>
2025-04-10 22:25:30 +0400

    chore: remove mock() method from BotClient

--- packages/bot/src/bot.ts
+++ packages/bot/src/bot.ts
@@ -95,40 +95,6 @@ export class BotClient {
   async stop() {
     await this.bot.stop()
   }
-
-  mock(isTesting: boolean) {
-    this.bot.botInfo = {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: this.bot.token,
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      can_connect_to_business: true,
-      has_main_web_app: true,
-      supports_inline_queries: false,
-    }
-
-    this.bot.api.config.use((prev, method, payload) => {
-      if (!isTesting) {
-        console.log(
-          `\n[BotClient][${this.bot.botInfo.username}] bot.${method}(${JSON.stringify(payload)})\n`,
-        )
-      }
-
-      if (method === 'getUpdates') {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            // biome-ignore lint/suspicious/noExplicitAny: Fake response
-            resolve({ ok: true, result: [] } as any)
-          }, 30000)
-        })
-      }
-
-      // biome-ignore lint/suspicious/noExplicitAny: Fake response
-      return Promise.resolve({ ok: true } as any)
-    })
-  }
 }
 
 interface ErrorResponse {
